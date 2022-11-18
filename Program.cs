a239a8ccbcf8dece9e8fae504cd01290f9a8638d
ashwin333 <ashwinpatil832@gmail.com>
2022-11-18 17:03:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    for (int i=1; i<=5;i++)
+            //    {
+            //      for (int j=i;j <=5;j++)
+            //         {
+            //            Console.Write("*");
+            //         }
+            //           Console.WriteLine();
+            //    }
+
+
+            //for (int i=1; i<=5;i++)
+            //{
+            //  for (int j=5;j>=1; j--)
+            //  {
+            //    Console.Write("*");
+            //  }
+            //   Console.WriteLine();
+            //}
+
+            //for (int i=1; i<=5;i++)
+            //{
+            //  for (int j=1;j<=1; j++)
+            //  {
+            //    Console.Write(j);
+            //  }
+            //   Console.WriteLine();
+            //}
+
+        }
+
+    }
+
+}
